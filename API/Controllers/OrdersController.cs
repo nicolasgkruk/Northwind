10c81283db68a62a50d50d48a4448190d73b9879
Nicolás Kruk <nicolasgkruk@gmail.com>
2019-05-10 15:58:01 +0200

    Removed dummy "ValuesController" and scaffolded API EF ProductsController, CustomersController and OrdersController

---
+++ API/Controllers/OrdersController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DB;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly ModelNorthwind _context;
+
+        public OrdersController(ModelNorthwind context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Orders
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Orders>>> GetOrders()
+        {
+            return await _context.Orders.ToListAsync();
+        }
+
+        // GET: api/Orders/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Orders>> GetOrders(int id)
+        {
+            var orders = await _context.Orders.FindAsync(id);
+
+            if (orders == null)
+            {
+                return NotFound();
+            }
+
+            return orders;
+        }
+
+        // PUT: api/Orders/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutOrders(int id, Orders orders)
+        {
+            if (id != orders.OrderID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(orders).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrdersExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Orders
+        [HttpPost]
+        public async Task<ActionResult<Orders>> PostOrders(Orders orders)
+        {
+            _context.Orders.Add(orders);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetOrders", new { id = orders.OrderID }, orders);
+        }
+
+        // DELETE: api/Orders/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Orders>> DeleteOrders(int id)
+        {
+            var orders = await _context.Orders.FindAsync(id);
+            if (orders == null)
+            {
+                return NotFound();
+            }
+
+            _context.Orders.Remove(orders);
+            await _context.SaveChangesAsync();
+
+            return orders;
+        }
+
+        private bool OrdersExists(int id)
+        {
+            return _context.Orders.Any(e => e.OrderID == id);
+        }
+    }
+}
