499b385109cf873648ec822c24c1e5ecfe0759fe
Nicolás Kruk <nicolasgkruk@gmail.com>
2019-05-10 15:28:43 +0200

    Removed Core MVC Web Application, added Net Core API instead

--- API/Program.cs
+++ API/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace AplicacionCore
+namespace API
 {
     public class Program
     {
